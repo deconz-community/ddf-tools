3d36be0c004d2116838645053f8205a4dfaedbdb
Zehir <zehir@zorim.fr>
2023-02-17 16:41:23 +0000

    add codemirror

---
+++ packages/playground/src/modules/codemirror.ts
@@ -0,0 +1,23 @@
+import { basicSetup } from 'codemirror'
+import VueCodemirror from 'vue-codemirror'
+import { javascript } from '@codemirror/lang-javascript'
+import { json } from '@codemirror/lang-json'
+
+import { oneDark } from '@codemirror/theme-one-dark'
+
+import { type UserModule } from '~/types'
+
+// Setup Json Viewer
+// https://www.npmjs.com/package/vue-json-viewer
+export const install: UserModule = ({ app }) => {
+  app.use(VueCodemirror, {
+    // optional default global options
+    autofocus: false,
+    disabled: false,
+    indentWithTab: true,
+    tabSize: 2,
+    placeholder: 'Code goes here...',
+    extensions: [basicSetup, javascript(), json(), oneDark],
+    // ...
+  })
+}
