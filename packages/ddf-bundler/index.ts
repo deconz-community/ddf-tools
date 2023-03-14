830442dbaa961ec5816d87d734cce6560ecbd6a6
Zehir <zehir@zorim.fr>
2023-03-14 12:28:03 +0000

    move directory

--- packages/ddf-bundler/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './src/const'
-export * from './src/bundle'
-export * from './src/types.d'
-
-export * from './src/builder'
-export * from './src/encoder'
-export * from './src/decoder'
-export * from './src/signer'
