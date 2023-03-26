15f02a901de0c0740583646b1aa210298f646c40
Zehir <zehir@zorim.fr>
2023-03-26 00:20:17 +0000

    update validation

--- packages/validator/src/schema/ddf.ts
+++ packages/validator/src/schema/ddf.ts
@@ -96,7 +96,7 @@ export function ddfBindingSchema(_generics: GenericsData) {
       'bind': z.literal('groupcast'),
       'src.ep': cf.endpoint().describe('Source endpoint.'),
       'cl': cf.hexa(4).describe('Cluster.'),
-      'config.group': z.number(),
+      'config.group': z.number().min(1).max(255),
     }),
   ])
 }
