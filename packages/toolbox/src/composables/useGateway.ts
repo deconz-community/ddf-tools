05b1daf74bb6d6455fd2ce1270eade0f291149b3
Zehir <zehir@zorim.fr>
2023-08-17 21:28:42 +0000

    Add type result lib

--- packages/toolbox/src/composables/useGateway.ts
+++ packages/toolbox/src/composables/useGateway.ts
@@ -1,10 +1,12 @@
 import type { Ref } from 'vue'
 import { assign, createMachine } from 'xstate'
 import { useMachine } from '@xstate/vue'
+import type { Result } from 'ts-results-es'
+import { Err, Ok } from 'ts-results-es'
 import type { GatewayCredentials } from '~/interfaces/deconz'
 
 export const gatewayMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5RQIYBcwHcUE8DEAqgA4TpgAEAxgE6RgB2aAligDawDaADALqKhEA9rCbNB9fiAAeiALQBGLgA4AdEoCs89QDYATOq6Gj2gDQgciAMwB2AL62zqDNhwqm9UXimw0ZFSgAzDGoACgMuAEo8JyxcNw80bj4kECERMQkUmQRZay4VAE55ABYuPXCjMrMLBF1dS3tHMhcVSnF6MEpmeig8CHEweIA3QQBrQbb6Dq6kyTTRJnFJbNlikpV5eV0C7XltUqVtbXVqq22Vfd0tDV1DAt07BxAYlsnp7t6wampBahUiVjoAK-AC2rXanUSvDmwgWSyyiEU6hUtwKSmsxWs8msNmKxSUpwQlnOl2u6luXHujyazjib0h7k+31+-0BaGB1DB9Jm8mSAlhGWWiA0hSUpUs2iUVylBWspnMiFK2hU6gle2s6kxml0Skaz2adIhXUZeC+Pz+AKBoPBU0hHF0fNSAsWmVA2U21hUxUsBnkSllDyUli4lkJxXuKhDu20Gq1W2p+tprkEAQCrHcYDwAGEjVDHfNBQiidYCpGCjYlFxNBrrEp5IT5Pc9S84im0xmVGaWQBXei0FCUAAWKAARqxMwAxJhSchMWCwbtgWYpAsuoUIJGWC7EsXB7HqWv1hVEq5qArn8v+-Zi4rNg3J1Ppjqd5l-dxDNhMCDkACCAAUAElyHGfApxnOcFyXaEV2deE3URKst20HdvS4fdDzDKs7yTFQ2yfQYuz+XtRnoQRMHoPAwNnedF2Xfl0jXItN23HVUPQuswwKfJ1Gw2IH3bZ9IAWHo+gGFQfD8Ft+PwzsIGEqA6KdBi4OkBD1FLYori49RqwPDjjxKLceKeKTcMfDshI+FR-yAkC8AAOTAKQ8xhZTXVUjdtRVLRjh09ijxqYpyRUGxjJpPizIEgi5KsmzgLAfA-1oIZFm7ThoPouF3PdXRkMjLhazYmt9JqCVlQecNLx2fE8V4lo8IsmLGWsiAIFoecHKclyYLc9ctC2bydB03SMOPMqUQxc9LCvGrbz1UiIDgSQpNcrL11kGN8iUKU630QlZDRfLDFueRLB2Ax1LquJ3FEVbC3gnI8hVSoXuMBs1hVK7XG5D47sYh6FE0FEjmsB4A30bFikJSw-UjK4-QPZQbDlL7cPofC-pUlY9F0NQdq2E4DMsYpPpM+9Ioxnq1qLHF8kMQq92KgK5EUZ7jr0oNtuDBNTIa585NgUdxwgTHssQawSxRPSir05mEAl1G+YI19RfXCXVHp3c0KZwl6hJs7z0lRRKx2MpFfMwTXxUXt+yHIWwFVosgy3fQGe12XodPf0L2m6qb3NqKX3NYZP2-OKQMdh6pWVTWZdGwKsLJnClaDnt6BIsj3NXLHhSCyMzspRmPePcNuIDmTLMZSOPK2LR8rd-zdeJCbKoKTVyTqeRy8a+TrMA+Kaky+6a+mmOCq1xuS64En0TCxMIpTyuehatq4HgKnh-dUf64nnWxsbFQJcq33r1qpOF4t6Le4AEQGaut8lHe45KxV-Q2fR7HsIA */
+  /** @xstate-layout N4IgpgJg5mDOIC5RQIYBcwHcUE8DEAqgA4TpgAEAxgE6RgB2aAligDawDaADALqKhEA9rCbNB9fiAAeiALQBGLgA4AdEoCs89QDYATOq6Gj2gDQgciAMwB2AL62zqDNhwqm9UXimw0ZFSgAzDGoACgMuAEo8JyxcNw80bj4kECERMQkUmQRZay4VAE55ABYuPXCjMrMLBF1dS3tHMhcVSnF6MEpmeig8CHEweIA3QQBrQZiWtvoOrvcoBHcRynQmcSSkyTTRNczQbIUlVXklEuKCy3VrawuC6sRi+pVLc9LipRejy21GkEm46azbq9fodYZjCbNAHtTrAxb0Zarda8DjyZICYQ7cSSA66eQFQr1bRlF5cPFXaz3BDqPFqbSWJTnSwneTWeTyX7-VyA2HzPoDcHjFRc1owuY9eGIjIbXTo1KYjI4uR1XQqbQnIpHUrk65U4nFQr0jTqR7ybRKbR2Bx-KHcsXA-lgpYQ4W20UzXkS50raUoyxy7aKrJyfHyQpcG6nUk6ynmRDaAr5YpG9QmvHmy2ct2CAIBVjuMB4ADC9s2KUDuyVCEsNZUXAu1iUXE01iupyp33yymsxQM1gzJyuWeccRzeYLKjA1GogmoKgArvRaChKAALFAAI1YhYAYkwpOQmLBYPOwGWMelK8GEIoI2rLLpGZYuKy2-IqZoCUoCj+Lt-tO8xTFMOsSuGO+ZglOM5zksbBMBA5AAIIAAoAJLkOM+B7geR4nmevBbAqV77Igt7WPej7FM+r6Nu+cYIMSDTWiK4ETlBs4LvQoz0IImD0Hg2GHsep7nvKl7Ytet7aCoxTyLoBT0ucBQGOoSh6kcKjqCBLSsZBEA7D0jqDD4fgsbmEGDJABlQKJFYSSRN69uReTvApGg3JYBSxjUD4GnolzKS8CbMgU2mjuZbH6cCKgoehmF4AAcmAUiJAR5ZEfZ0ikeoRSaVo2ipi2b56gYahAUoNyaE21j1GFYERXp1kxWhGFgPgyG0EMazzpwaUXliexZTeLyqoYjZUS+ra0R+AEqD27zWC8JSppmzHZg1llRfMMUQBAtDHolyWpQGGWDdkWi5ZoOiFTR7b0ToBrzRVS29joVrWjxEBwJIXKEeJZ1yJa+RHI+cnqFSsgFEodXxKIf0DVWuT5BUlSo6Y9ElGGWlrSOdoeuKUDw0GDkKJoKi6Nolq6DcNX6KyxQdicdZ4oOeQfNcPw46BKjiBZRPEUNsh6KqIOnPoVLyC8mkw7pYD85l2QFQaY1PpNxX0QoyMyxtKj6bAm7bhA8sAwg1wEvo43UVNd01Nc2vjpB06zsbVZ5KNEaq7ddE1FRqqeb+DIKYBwFczpOvsXOi7LmuBty+l-2u1c5NthNXsfviai-n+QeMiHTS4zz4dOzBCJwQhsWtTU-XE0NrZdh7qfW978ZcEx+fc7Lk7F5x3G8YNdkm-2BKt4mFxq9N9Hmqo2Pt2HDubdZLuSfJysN1b6s+8+c3lYtsmvZTM82gXndWdFFeYUvDklAVdZr+PNst5Yzw78pJwRrVofhfPk5bT0O17XAeA8cEaSWTOoW+lt77N2pEzXQO8XorStLPL+Fkf5NQACIDEvkNM45EVaNw3vGCqmkCgLVftVD+9ggA */
   id: 'gateway',
   predictableActionArguments: true,
 
@@ -17,10 +19,10 @@ export const gatewayMachine = createMachine({
     },
     services: {} as {
       connectToGateway: {
-        data: {
-          api: string
-          websocket: string
-        }
+        data: Result<{
+          api: string | null
+          websocket: string | null
+        }, 'invalid_api_key' | 'unreachable' | Error>
       }
     },
   },
@@ -34,14 +36,13 @@ export const gatewayMachine = createMachine({
 
     connecting: {
       invoke: {
-        id: 'connect',
         src: 'connectToGateway',
-        onDone: {
+
+        onDone: [{
           target: 'online',
           actions: ['useURIs'],
-        },
-
-        onError: [{
+          cond: 'isOk',
+        }, {
           target: 'offline.error.unreachable',
           cond: 'isUnreachable',
         }, {
@@ -59,11 +60,7 @@ export const gatewayMachine = createMachine({
 
         error: {
           states: {
-            'unreachable': {
-              on: {
-                'Fix issue': '#gateway.offline.editing.Address',
-              },
-            },
+            'unreachable': {},
             'invalid API key': {
               on: {
                 'Fix issue': '#gateway.offline.editing.API key',
@@ -101,7 +98,7 @@ export const gatewayMachine = createMachine({
 
           initial: 'Address',
 
-          onDone: '#gateway.init',
+          onDone: '#gateway.connecting',
         },
       },
 
@@ -122,18 +119,22 @@ export const gatewayMachine = createMachine({
 }, {
   services: {
     connectToGateway: async (context, event) => {
-      return { api: '', websocket: '' }
+      if (Math.random() > 0.5)
+        return Err('unreachable' as const)
+      return Ok({ api: null, websocket: null })
     },
   },
   actions: {
     useURIs: assign({
-      uri_api: (context, event) => event.data.api,
-      uri_websocket: (context, event) => event.data.websocket,
+      uri_api: (context, event) => event.data.unwrap().api,
+      uri_websocket: (context, event) => event.data.unwrap().websocket,
     }),
   },
   guards: {
-    isInvalidAPIKey: () => false,
-    isUnreachable: () => false,
+    isOk: (context, event) => event.data.isOk(),
+    // isErr: (context, event) => event.data.isErr(),
+    isInvalidAPIKey: (context, event) => event.data.isErr() && event.data.unwrapErr() === 'invalid_api_key',
+    isUnreachable: (context, event) => event.data.isErr() && event.data.unwrapErr() === 'unreachable',
   },
 })
 
