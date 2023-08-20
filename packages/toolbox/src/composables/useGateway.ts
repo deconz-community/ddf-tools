7c9b3dd9c59ab2d7153f132e5becd6d654e2e3e0
Zehir <zehir@zorim.fr>
2023-08-20 09:20:47 +0000

    code sync

--- packages/toolbox/src/composables/useGateway.ts
+++ packages/toolbox/src/composables/useGateway.ts
@@ -5,7 +5,11 @@ import { Err, Ok } from 'ts-results-es'
 import { Gateway } from '@deconz-community/rest-client'
 import type { GatewayCredentials } from '~/interfaces/deconz'
 
-const defaultContext = {
+const defaultContext: {
+  credentials: GatewayCredentials
+  gateway?: ReturnType<typeof Gateway>
+  devices_list: string[]
+} = {
   credentials: {
     apiKey: '<nouser>',
     id: '<unknown>',
@@ -15,23 +19,19 @@ const defaultContext = {
       websocket: [],
     },
   },
-  uri_api: null,
-  uri_websocket: null,
+  gateway: undefined,
+  devices_list: [],
 }
 
 export const gatewayMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5RQIYBcwHcUE8DEAqgA4TpgAEAxgE6RgB2aAligDawDaADALqKhEA9rCbNB9fiAAeiALQBmAJwA6AEwAWRVsUBWAGx75ARlVH5AGhA5EigOzLbt+evUAOLotfquR-QF8-S1QMbBxlJnpRPClYNDJlFAAzDGoACh0uLgBKPGCsXHDItG4+JBAhETEJMpkEWSM9FVdVWx1bPR95HTM9dUtrBFVVFSMjV0UxifGx11sAoLJQ5UpxejBKZnooPAhxMEKAN0EAa328pZX6NY2IqAQIo8p0JnESkskK0Rfq0FrZDNcyjGRhcii6jjBin6iHkqi4akU6j0LXkzWa6nk8nmIHOBUu102212a0OJzOizxq3WhPu9Eez1evA4RlKAmEX3Ekj+pnkyj0Tg6hm8pjatmhCBc6mU8kMXjBwNso2xuLC+Optx2e1Jp2UKuWVJuW1p9Kqb1UrPK7KqXLkpj00r001cXi4Isc4p0LWUSNROh06jtrn5yopqoNhM1JIeZN1of1V3VRujT1NTPkFs+1pqcjGOj5bU8ZmFvndVkQehMykUsr9AYaQbmgRxccEiUSrAiYDwAGFw+8ypnvjaEJjeR55LZ3N1WpOjOKZfCuLN1Bl2kHc42FiECq3253lGBqNRBNRDmwmBByABBAAKAElyKd8AAxJhSchMWCwACuYH7bMqIdswQOxAWdVQvHkHwZ1cOcywQT08x0ENtzCXcOxJQ9j1Pb96GOehBEweg8Ffd9Px-P9eA+K0gN+GxHD5ZFq2cLQMh0VxxQrIwUPyNC2ww-ZIC+LZlFvB8nzwAA5MApGKKiBxozlgJBDI1D0fQ-WnNpYI9JxpRcWZdDGLhbFULEmz1dD9yEwlRPvR8wHwG9aAOF5v04eSAI5H5pEQExMgcbSMWg7S4IGNx7A0CLnBU-R1J4pYrMwiBhKgUSIAgWgvykmS5IzRSfNqFT4VUdT1L9IwYLCxAIrUAynHUWKDGQiyW346yjxPZRcNoFBKAACxQAAjVgu1Ij8v1-f9LUApS6JAydlHA1xukdUyDGqiUA29TJXUnRrkX8bECIgOBJBVajZsKuRYJUQwIJMHRxVkTwEoKCJREu7zh3qWx4QyXbAcBvRxX85QWq3Xj4wJW4vqzeb6m6NT+RaOwhmnPp4OMQFXVGFa-tcCd2jetD6AEuHaN8upHCMPlYVg1QnqxxrwdMSqkS0WxNGrEnlCSsAKbmqmDBKoKoMq0LnqMf7ef55QUtgYbRogQXroQCE1DFkLZ3FRxZfazDOuoVXhwJzX9vFqr539KtdpBZFpaUbjWtQvmDcEo3uvoXqBqVgWFKu03DHBkzIO1nT4JcVRpW0MFxl6Lx1H1vdDews8O0vMSHIGLz4ap1p7UZi3w82xDweTgSD093D8MInzByF2pHDzCDFHHEvOKVF2oblmzYYD77lN8aOYOCiWdaxnReSirwGgyRnkT0CvrJS2ys6fE3lNhXlR8tyX4LLxPDO6dxTPMyHEvdg9V9udLMrgeAB7zort8C4vx4j8LvAceqYpXOKIbNldr3G+IkAAiexN7zTMFwPMu8O7wQrIuREx9jJnwCAEIAA */
+  /** @xstate-layout N4IgpgJg5mDOIC5RQIYBcwHcUE8DEAqgA4TpgAEAxgE6RgB2aAligDawDaADALqKhEA9rCbNB9fiAAeiALQBmAJwA6AEwAWRVsUBWAGx75ARlVH5AGhA5EigOzLbt+evUAOLotfquR-QF8-S1QMbBxlJnpRPClYNDJlFAAzDGoACh0uLgBKPGCsXHDItG4+JBAhETEJMpkEWSM9FVdVWx1bPR95HTM9dUtrBFVVFSMjV0UxifGx11sAoLJQ5UpxejBKZnooPAhxMEKAN0EAa328pZX6NY2IqAQIo8p0JnESkskK0Rfq0FrZDNcyjGRhcii6jjBin6iHkqi4akU6j0LXkzWa6nk8nmIHOBUu102212a0OJzOizxq3WhPu9Eez1evA4RlKAmEX3Ekj+pnkyj0Tg6hm8pjatmhCBc6mU8kMXjBwNso2xuLC+Optx2e1Jp2UKuWVJuW1p9Kqb1UrPK7KqXLkpj00r001cXi4Isc4p0LWUSNROh06jtrn5yopqoNhM1JIeZN1of1V3VRujT1NTPkFs+1pqcjGOj5bU8ZmFvndVkQehMykUsr9AYaQbmgRxcfErAi+yYEFYYGisXiSRSqSMmRHmRyetb7fCXbA7zKme+Nrqw9dyh04q6ec9o1sLka-q4ehDIQKk5JAAVBII21tyBAwAcmJQ4JGO3SYxP6Df9pfr7c7w+T5wMaggpt8by8B8VqLtmy6uioShePIrQelwvLbkYu5Iro3hHk2E6JIk354AAwuGc5spUMG-DCmLKB4yHuN0rS2K4Rgbh09GzOoGTtEGuaNgsJ5hIIhHfsoYDUNQgjUMoACu9C0CglAABYoAARt2eAAGJMFI5BMLAsBybOkHztBnKwXYgLOqorjdI6tiqAY7FlhKAbeiOLReA0qj+PhLZiVOknSbJDxsJ25AAILngAkuQpz4Lp+mGcZpkZhZPzSDYrHKLZSE+CxbEen5a7HvkIlBSSIUyfJ9DHPQgiYPQOl6QZRkmRRlpUZZNEIHY9gGMMQpaBkOiuOKFZGOVSyiURwUQF8WzKDF8WJXgAByYBSMUZmURyWW1CCGRqHo+h+sxbTFW5bS8s4bi2LoYxcE5WIBcJyhzeJkBLVAK1xQlYD4OetCPoIcmcHt3UHUuJiZA4V0YoVV2uQMD1qC4szOMd+hnTNp5VfsP2EitEAQLQRmbdtu0ZT1h2IMd8LOedfqYSj4roxoD3YzxuM6PjIlflOnZaQASmAiQUypAGPs+kO0zDsH1K69oeL49keGhFZ6OKwz2BhWH7rhARNo197wGUKpQXTS71H5vKGHZJjrm5siePRo6oiCjROYJzYfREojW4rfX1LY8IZKOUeZDrblw2V70VfGBK3MHWah74RinfyLR2EMzF9G5xiAq6oz2eHrjIe0AufULaxp9R2XLnCeaO2xfm68O9GmJhe44YeNdnh2M4N71Tf1CWfKwu3LsDF0Uql732EHnhQlJ0Pyi-jeUAy0BFv7en4-2a30-Oxu-rdzufcr4PhOj-TCAGEziPyMjrGozmEee2x2FOQYt-zRJItWAGluwQHvkuCEagX5v2ugMRwADvpSRkhA2CFdoGsSRmzd+58pTjG0KiasbgXCIOCsg2SCklKqVAWAVBfV2joRegVbBcDEAuFUNKbQYJxi9C8OoUh1VyGHAihAaKANEp0Kbq0e0flMGvxYR-BAnotwCKJkIhSDUmpZQXGPWojg8x2UUAxWBiipqqIkotQkkijq+A4UVLBRVFFdF5FzLG6gcYGH5onWahMLG-X+mtIG1iGawl5PY+RjiSp5j4bMJ67hXrmOJrcUm5M4D72hofI6oSEZyJMRzbwDhMZOHcbzTxiTLHJIACJ7GCQgMwXA8zhLyW5Cs8IPAPTiS9VQb0AhAA */
   id: 'gateway',
   predictableActionArguments: true,
   tsTypes: {} as import('./useGateway.typegen').Typegen0,
   schema: {
-    context: {} as {
-      credentials: GatewayCredentials
-      uri_api: string | null
-      uri_websocket: string | null
-    },
+    context: {} as typeof defaultContext,
     events: {} as | {
-      type: 'Fix issue' | 'Done' | 'Next' | 'Previous' | 'Connect'
+      type: 'Fix issue' | 'Done' | 'Next' | 'Previous' | 'Connect' | 'Refresh devices'
     } | {
       type: 'Update credentials'
       data?: GatewayCredentials
@@ -39,12 +39,14 @@ export const gatewayMachine = createMachine({
     services: {} as {
       connectToGateway: {
         data: Result<{
-          api: string | null
-          websocket: string | null
+          gateway: ReturnType<typeof Gateway>
         }, {
           type: 'invalid_api_key' | 'unreachable' | 'unknown'
           message?: string
         }>
+      }
+      fetchDevices: {
+        data: Result<string[], never>
       }
     },
   },
@@ -63,8 +65,8 @@ export const gatewayMachine = createMachine({
         src: 'connectToGateway',
 
         onDone: [{
-          target: 'online',
-          actions: ['useURIs'],
+          target: 'online.Pooling devices',
+          actions: ['useClient'],
           cond: 'isOk',
         }, {
           target: 'offline.error.unreachable',
@@ -76,7 +78,32 @@ export const gatewayMachine = createMachine({
       },
     },
 
-    online: {},
+    online: {
+      states: {
+        'idle': {
+          on: {
+            'Refresh devices': 'Pooling devices',
+          },
+
+          after: {
+            10000000: 'Pooling devices',
+          },
+        },
+
+        'Pooling devices': {
+          invoke: {
+            src: 'fetchDevices',
+            onDone: {
+              target: 'idle',
+              actions: 'updateDeviceList',
+            },
+          },
+        },
+      },
+
+      initial: 'idle',
+      exit: 'cleanUpDevices',
+    },
 
     offline: {
       states: {
@@ -153,8 +180,8 @@ export const gatewayMachine = createMachine({
 
       const queries = context.credentials.URIs.api.map(async (api) => {
         try {
-          const client = Gateway(api, context.credentials.apiKey)
-          const config = await client.client.getConfig()
+          const gateway = Gateway(api, context.credentials.apiKey)
+          const config = await gateway.client.getConfig()
 
           if (!config.success)
             throw new Error('No response from the gateway')
@@ -178,10 +205,7 @@ export const gatewayMachine = createMachine({
           }
 
           resolved = true
-          resolve(Ok({
-            api,
-            websocket: context.credentials.URIs.websocket[0],
-          }))
+          resolve(Ok({ gateway }))
         }
         catch (e) {
           return Err({
@@ -218,14 +242,25 @@ export const gatewayMachine = createMachine({
         } as const))
       })
     }),
+
+    fetchDevices: context => new Promise((resolve) => {
+      context.gateway?.client.getDevices().then((result) => {
+        resolve(Ok(result.success!))
+      })
+    }),
   },
   actions: {
-    useURIs: assign({
-      uri_api: (context, event) => event.data.unwrap().api,
-      uri_websocket: (context, event) => event.data.unwrap().websocket,
+    useClient: assign({
+      gateway: (context, event) => event.data.unwrap().gateway,
     }),
     updateCredentials: assign({
       credentials: (context, event) => event.data ?? structuredClone(defaultContext.credentials),
+    }),
+    updateDeviceList: assign({
+      devices_list: (context, event) => event.data.unwrap(),
+    }),
+    cleanUpDevices: assign({
+      devices_list: [],
     }),
   },
   guards: {
--- packages/toolbox/src/pages/test.vue
+++ packages/toolbox/src/pages/test.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 const gatewayStore = useGatewaysStore()
 
+console.log(import.meta.env.VITE_GATEWAY_CREDENTIALS)
+
 const creds = JSON.parse(import.meta.env.VITE_GATEWAY_CREDENTIALS)
 
 const gateway = computed(() => {
