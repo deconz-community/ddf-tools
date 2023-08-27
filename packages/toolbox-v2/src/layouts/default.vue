10ca44ee47970ba3b8c8d6e7544f6fe23883d757
Zehir <zehir@zorim.fr>
2023-08-27 14:01:11 +0000

    move layout

--- packages/toolbox-v2/src/layouts/default.vue
+++ packages/toolbox-v2/src/layouts/default.vue
@@ -1,12 +1,18 @@
 <template>
-  <main
-    px-4 py-10
-    text="center gray-700 dark:gray-200"
-  >
-    <RouterView />
-    <TheFooter />
-    <div mx-auto mt-5 text-center text-sm opacity-50>
-      [Default Layout]
-    </div>
-  </main>
+  <v-layout>
+    <nav-topbar-global />
+    <nav-sidebar-level-one />
+    <nav-topbar-details />
+
+    <portal-target name="before-content" />
+    <!--
+    <perfect-scrollbar>
+      -->
+    <v-main>
+      <router-view />
+    </v-main>
+    <!--
+    </perfect-scrollbar>
+    -->
+  </v-layout>
 </template>
