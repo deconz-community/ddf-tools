9ee013e44522ef780327a2a9c115ecc98be1f2f6
Zehir <zehir@zorim.fr>
2023-08-27 14:07:22 +0000

    import modules

---
+++ packages/toolbox-v2/src/modules/perfect-scrollbar.ts
@@ -0,0 +1,9 @@
+import PerfectScrollbar from 'vue3-perfect-scrollbar'
+import 'vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css'
+import type { UserModule } from '~/types'
+
+// Setup Perfect Scrollbar
+// https://github.com/mercs600/vue3-perfect-scrollbar
+export const install: UserModule = ({ app }) => {
+  app.use(PerfectScrollbar)
+}
