6ce567d728995c6c5413af912c1dee2e7193dcbc
Zehir <zehir@zorim.fr>
2023-09-21 17:53:37 +0000

    cleanup

--- packages/cli/ddf-tools.js
+++ packages/cli/ddf-tools.js
@@ -4,7 +4,6 @@ import process from 'node:process'
 import { createRequire } from 'node:module'
 import { program } from 'commander'
 
-import { bundler } from './src/bundler.js'
 import { validator } from './src/validator.js'
 
 const packageDefinition = createRequire(import.meta.url)('./package.json')
@@ -14,7 +13,6 @@ program
   .version(packageDefinition.version)
   .description('A command line tool for working with DDFs.')
 
-bundler()
 validator()
 
 program.parse(process.argv)
