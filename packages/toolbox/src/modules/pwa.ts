856ebcc3a0c4b6c2f8bb69858163a5c49a3d40a7
Zehir <zehir@zorim.fr>
2023-11-29 22:16:51 +0000

    Fix PWA

--- packages/toolbox/src/modules/pwa.ts
+++ packages/toolbox/src/modules/pwa.ts
@@ -1,9 +1,11 @@
-import { type UserModule } from '~/types'
+import type { UserModule } from '~/types'
 
 // https://github.com/antfu/vite-plugin-pwa#automatic-reload-when-new-content-available
 export const install: UserModule = ({ router }) => {
-  router.isReady().then(async () => {
-    const { registerSW } = await import('virtual:pwa-register')
-    registerSW({ immediate: true })
-  })
+  router.isReady()
+    .then(async () => {
+      const { registerSW } = await import('virtual:pwa-register')
+      registerSW({ immediate: true })
+    })
+    .catch(() => {})
 }
--- packages/toolbox/vite.config.ts
+++ packages/toolbox/vite.config.ts
@@ -111,17 +111,17 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: '/ddf-tools/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/ddf-tools/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: '/ddf-tools/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
