f274b407e178050f64c1e521ea48c5a672c717fb
Zehir <zehir@zorim.fr>
2023-12-12 18:40:48 +0000

    cleanup

--- packages/toolbox/src/pages/sandbox.vue
+++ packages/toolbox/src/pages/sandbox.vue
@@ -1,29 +1,5 @@
 <script setup lang="ts">
-import { readBundles } from '~/interfaces/store'
-
 const store = useStore()
-
-const bundleID = ref('3a20ecba2f1294dbea57fa10b0383775606ab5c7ec77f46775eb49ecdd93a5bd')
-
-const dataRest = store.request(computed(() => readBundles(bundleID.value, {
-  fields: [
-    'id',
-    'product',
-    'version',
-    '*',
-    {
-      device_identifiers: [
-        'id',
-        {
-          device_identifiers_id: [
-            'manufacturer',
-            'model',
-          ],
-        },
-      ],
-    },
-  ],
-})))
 </script>
 
 <template>
@@ -34,11 +10,9 @@ const dataRest = store.request(computed(() => readBundles(bundleID.value, {
 
     <template #text>
       <pre>{{ store.state?.value }}</pre>
-      <v-text-field v-model="bundleID" label="Bundle ID" />
       <!--
       <pre>{{ store.state?.context.profile }}</pre>
       -->
-      <pre>{{ dataRest }}</pre>
     </template>
   </v-card>
 </template>
