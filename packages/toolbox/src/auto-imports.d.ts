36d442ffb1395a675db3bf3355dcb2fd284b722e
Zehir <zehir@zorim.fr>
2024-02-20 11:21:21 +0000

    Add GlobalComponents interface to vue and @vue/runtime-core modules

--- packages/toolbox/src/auto-imports.d.ts
+++ packages/toolbox/src/auto-imports.d.ts
@@ -316,6 +316,7 @@ declare global {
 // for vue template auto import
 import { UnwrapRef } from 'vue'
 declare module 'vue' {
+  interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly $$: UnwrapRef<typeof import('vue/macros')['$$']>
     readonly $: UnwrapRef<typeof import('vue/macros')['$']>
@@ -617,6 +618,7 @@ declare module 'vue' {
   }
 }
 declare module '@vue/runtime-core' {
+  interface GlobalComponents {}
   interface ComponentCustomProperties {
     readonly $$: UnwrapRef<typeof import('vue/macros')['$$']>
     readonly $: UnwrapRef<typeof import('vue/macros')['$']>
