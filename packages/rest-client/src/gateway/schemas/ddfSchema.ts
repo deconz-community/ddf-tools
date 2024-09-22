74fdde8908c36602e44ca5a7f70e824af67b1cb9
Zehir <zehir@zorim.fr>
2024-09-22 20:21:05 +0000

    add descriptions

--- packages/rest-client/src/gateway/schemas/ddfSchema.ts
+++ packages/rest-client/src/gateway/schemas/ddfSchema.ts
@@ -1,13 +1,12 @@
 import { z } from 'zod'
 
 export const ddfdDescriptorSchema = z.strictObject({
-  uuid: z.string(),
-  product: z.string(),
-  version_deconz: z.string(),
-  last_modified: z.string().transform(value => new Date(value)),
-  file_hash: z.string().optional()
-    .describe('SHA256 hash of the file since version 2.27.1'),
+  uuid: z.string().describe('UUID of the source DDF. It\'s used to find other version of the DDF on the bundle store.'),
+  product: z.string().describe('Product name of the device.'),
+  version_deconz: z.string().describe('Minimum version of the deCONZ REST API.'),
+  last_modified: z.string().transform(value => new Date(value)).describe('Last modified date of the DDF source.'),
+  file_hash: z.string().optional().describe('SHA256 hash of the file since version 2.27.1'),
   device_identifiers: z.array(
     z.tuple([z.string(), z.string()]),
-  ),
+  ).describe('List of device identifiers that are supported by the DDF bundle.'),
 }).passthrough()
